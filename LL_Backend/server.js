66f13c3e2af3a444684b9e16592f50d0767377e6
micaiahcape <50351592+micaiahcape@users.noreply.github.com>
2024-05-19 22:51:21 -1000

    schema commit test

--- LL_Backend/server.js
+++ LL_Backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -12,7 +13,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.post("/", (req, res) => {
+  res.send("Recieved");
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+const hashtag = new mongoose.Schema({
+  name: String,
+  ratings: [Number],
+})
+
+const Hashtag = mongoose.model('Hashtag', hashtag);
